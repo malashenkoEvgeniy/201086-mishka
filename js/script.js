e537ad078165e6a87f24a8fb8916db603df62bae
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-10-01 00:04:58 +0300

    adds heder

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var link = document.querySelector(".main-nav__togle");
+var popupList = document.querySelector(".site-list");
+
+link.addEventListener('click', function () {
+  link.classList.toggle("main-nav__togle-close");
+  popupList.classList.toggle("site-list__active")
+});
