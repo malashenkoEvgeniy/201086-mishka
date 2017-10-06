90230f6778a1439cd3e2361d08cb37e266d2605b
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-10-06 20:21:18 +0300

    chengs style block

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ var link = document.querySelector(".main-nav__togle");
 var popupList = document.querySelector(".site-list");
 
 link.addEventListener('click', function () {
-  link.classList.toggle("main-nav__togle-close");
-  popupList.classList.toggle("site-list__active")
+  link.classList.toggle("main-nav__togle--close");
+  popupList.classList.toggle("site-list--active")
 });
