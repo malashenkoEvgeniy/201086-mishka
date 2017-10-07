d6e42e6ca9854a912f480e4a3edd39a5db87c964
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-10-07 07:54:00 +0300

    adds style linter

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ var popupList = document.querySelector(".site-list");
 
 link.addEventListener('click', function () {
   link.classList.toggle("main-nav__togle--close");
-  popupList.classList.toggle("site-list--active")
+  popupList.classList.toggle("site-list--active");
 });
